ab05eeef8d8eea95521b6e3e6fb468dcab61215e
PeterKenny-10 <niamhaustin@gmail.com>
2021-03-18 20:39:58 +0000

    18th of March

--- Olympics/Oly trial.py
+++ Olympics/Oly trial.py
@@ -8,12 +8,9 @@ print(df.head)
 
 print(df.info())
 
-
-
-
-
-brics = pd.read_csv("Olympic_medals.csv", index_col=0)
-print(brics)
+print(df[df.isnull().any(axis=1)])
+df.dropna(axis=0,inplace=True )
+print(df.shape)
 
 
 
@@ -39,6 +36,9 @@ print(Ireland_sex)
 
 import matplotlib.pyplot as plt
 Ireland_medals.plot(kind='bar')
+plt.title('Irelands Olympic medals',fontsize=18, color='red')
+plt.xlabel('Year')
+plt.ylabel('Medals')
 plt.show()
 
 
@@ -50,15 +50,33 @@ plt.show()
 
 United_States = df[df['Country']== 'United States']
 United_States_medals = United_States.groupby('Year')['Medal'].count()
-print(United_States_medals)
+
 
 Spain = df[df['Country']== 'Spain']
 Spain_medals = Spain.groupby('Year')['Medal'].count()
-print(Spain_medals)
+
 
 hosts ={'Montreal':'Canada','Moscow':'USSR','Los Angeles':'USA', 'Soeul':'South Korea','Barcelona':'Spain','Atlanta':'USA','Sydney':'Australia','Athens':'Greece','Beijing':'China'}
 for key, value in hosts.items():
     print('The host city of '+str(key)+ ' is in '+ str(value))
+
+print(df.iloc[10000:10050,1:4])
+
+UK_Ireland=['Ireland', 'United Kingdom']
+df1= df[df['Country'].isin(UK_Ireland)]
+
+UK_Ireland_Woman=df1.loc[df['Gender']=='Women']
+UK_Ireland_Men=df1.loc[df['Gender']=='Men']
+print(UK_Ireland_Woman.head())
+print(UK_Ireland_Men.head())
+
+plt.figure(figsize=(12,5))
+plt.legend(labels=['UK/Ireland Men', 'Uk/Ireland Woman'])
+plt.xlabel('Years')
+plt.ylabel('Count of Athletes (Women/Men')
+plt.show()
+
+
 
 
 
